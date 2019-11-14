c49c7b6b722e381b1a9b06a0d161a3f03acc6034
mac <aaron.asencio.tavio@gmail.com>
2019-11-14 12:00:29 +0000

    add login view

---
+++ app/src/main/java/org/sic4change/chuqabp/CreateAccountFragment.kt
@@ -0,0 +1,45 @@
+package org.sic4change.chuqabp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CreateAccountFragment: Fragment() {
+
+
+    /**
+     * Called when the fragment's activity has been created and this
+     * fragment's view hierarchy instantiated.  It can be used to do final
+     * initialization once these pieces are in place, such as retrieving
+     * views or restoring state.
+     */
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        (activity as LoginActivity).supportActionBar?.title = getString(R.string.app_name)
+    }
+
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     *
+     * <p>If you return a View from here, you will later be called in
+     * {@link #onDestroyView} when the view is being released.
+     *
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return Return the View for the fragment's UI.
+     */
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_create_account, container, false)
+        return view
+    }
+
+
+}

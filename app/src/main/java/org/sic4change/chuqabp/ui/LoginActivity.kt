2121b4904fc663145ebc3df825266caf7faa66d9
mac <aaron.asencio.tavio@gmail.com>
2019-11-26 11:52:07 +0000

    show name, surnames,email and photo user in user fragment

--- app/src/main/java/org/sic4change/chuqabp/ui/LoginActivity.kt
+++ app/src/main/java/org/sic4change/chuqabp/ui/LoginActivity.kt
@@ -11,12 +11,12 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        val navController = this.findNavController(R.id.my_nav_host_fragment)
+        val navController = this.findNavController(R.id.nav_host_fragment_login)
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.my_nav_host_fragment)
+        val navController = this.findNavController(R.id.nav_host_fragment_login)
         return navController.navigateUp()
     }
 }
--- app/src/main/java/org/sic4change/chuqabp/utils/ImageView.kt
+++ app/src/main/java/org/sic4change/chuqabp/utils/ImageView.kt
@@ -3,12 +3,13 @@ package org.sic4change.chuqabp.utils
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import org.sic4change.chuqabp.R
 
 /**
  * Methods to show ImageView
  */
-fun ImageView.loadUrl(url: String) {
-    Glide.with(context).load(url).into(this)
+fun ImageView.loadPhotoUrl(url: String?) {
+    Glide.with(context).load(url).placeholder(R.drawable.ic_camera).into(this)
 }
 
 

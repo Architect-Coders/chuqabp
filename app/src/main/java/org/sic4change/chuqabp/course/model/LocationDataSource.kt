8548f03a5526c7916ffc797e941e2a3a5ff749c8
mac <aaron.asencio.tavio@gmail.com>
2019-11-29 12:00:16 +0000

    solid

---
+++ app/src/main/java/org/sic4change/chuqabp/course/model/LocationDataSource.kt
@@ -0,0 +1,25 @@
+package org.sic4change.chuqabp.course.model
+
+import android.location.Location
+import android.annotation.SuppressLint
+import android.app.Activity
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+
+interface LocationDataSource {
+    suspend fun findLastLocation(): Location?
+}
+
+class PlayServicesLocationDataSource(activity: Activity) : LocationDataSource {
+    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity)
+
+    @SuppressLint("MissingPermission")
+    override suspend fun findLastLocation(): Location? =
+        suspendCancellableCoroutine { continuation ->
+            fusedLocationClient.lastLocation
+                .addOnCompleteListener {
+                    continuation.resume(it.result)
+                }
+        }
+}

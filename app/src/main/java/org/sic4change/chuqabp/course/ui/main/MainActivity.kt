067fc0728066e206def540c47bc397331b41ebbc
mac <aaron.asencio.tavio@gmail.com>
2020-03-02 09:53:23 +0000

    improve back navigation

--- app/src/main/java/org/sic4change/chuqabp/course/ui/main/MainActivity.kt
+++ app/src/main/java/org/sic4change/chuqabp/course/ui/main/MainActivity.kt
@@ -36,6 +36,8 @@ class MainActivity : AppCompatActivity() {
     override fun onBackPressed() {
         if (title.contains(":")) {
             finishAffinity()
+        } else {
+            super.onBackPressed()
         }
     }
 
